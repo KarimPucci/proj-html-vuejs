a13fa2de7ead52141b6e358169c7b6863b1caef8
Karim <kizpucci@gmail.com>
2022-11-24 15:53:33 +0100

    scaffolding

---
+++ proj-html-vuejs/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppJumbotron from "./components/AppJumbotron.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppJumbotron,
+    AppMain,
+    AppFooter,
+  },
+};
+</script>
+
+<template></template>
+
+<style>
+</style>
